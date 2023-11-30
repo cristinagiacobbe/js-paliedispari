931fe9d9eff7307f36d3715f409e4d7e8e3fe8d1
cristinagiacobbe <crigiak@gmail.com>
2023-11-30 22:10:59 +0100

    Define tools

---
+++ script.js
@@ -0,0 +1,13 @@
+
+//Chiedere all'utente di inserire una parola
+//creare una funzione per capire
+//se una parola è palindroma
+
+//TOOLS:
+//Prompt to ask user to choose a word
+//a check on this word (if)
+//a method to check the condition (reverse?)
+//a function
+
+
+
